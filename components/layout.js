530f535e891db582e28a759b1b85bb3185e5d702
Junyu Pu <junyu.pu@labtwin.com>
2020-05-19 09:40:08 +0200

    use localstorage

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,8 @@
-import React, { useState, useEffect } from "react";
-
 import styles from "./layout.module.css";
 
 function Layout({ children }) {
-  const [isDarkMode, setDarkMode] = useState(true);
-  useEffect(() => {
-    const userPrefersDark =
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-    setDarkMode(userPrefersDark);
-  }, []);
   return (
-    <div className={styles.wrapper} data-theme={isDarkMode ? "dark" : null}>
-      <div className={styles.switcher} onClick={() => setDarkMode(!isDarkMode)}>
-        {isDarkMode ? "🌝" : "🌚"}
-      </div>
+    <div className={styles.wrapper}>
       <div className={styles.container}>{children}</div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
 import "../styles/global.css";
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [isDarkMode, setDarkMode] = useState(null);
+  const handleSwitch = (value) => {
+    localStorage.setItem("darkTheme", value);
+    setDarkMode(value);
+  };
+  useEffect(() => {
+    const userPref = localStorage.getItem("darkTheme");
+    const systemPref =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (userPref === null) {
+      setDarkMode(systemPref);
+    } else {
+      setDarkMode(userPref);
+    }
+  }, []);
+  return (
+    <div data-theme={isDarkMode ? "dark" : null}>
+      <div className="switcher" onClick={() => handleSwitch(!isDarkMode)}>
+        {isDarkMode ? "🌝" : "🌚"}
+      </div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
